0d169fdeb34f45347aaf72211501ebaaf5f4e30e
thaiduy1704 <thaiduy1704@gmail.com>
2023-03-01 17:10:11 +0700

    Setup Backend Mongo

---
+++ server/routes/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { clientRouter } from "./v1/clientRouter.js";
+import { managementRouter } from "./v1/managementRouter.js";
+import { generalRouter } from "./v1/generalRouter.js";
+import { salesRouter } from "./v1/salesRouter.js";
+
+const rootRouter = express.Router();
+
+rootRouter.use("/client", clientRouter);
+rootRouter.use("/general", generalRouter);
+rootRouter.use("/management", managementRouter);
+rootRouter.use("/sales", salesRouter);
+
+export { rootRouter };
