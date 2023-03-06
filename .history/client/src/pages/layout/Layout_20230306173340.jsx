a2bbe391f74a710d4307b8a423b4bfe3d41f1c58
thaiduy1704 <thaiduy1704@gmail.com>
2023-03-06 17:33:49 +0700

    SideBar Done

---
+++ .history/client/src/pages/layout/Layout_20230306173340.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { Box, useMediaQuery } from "@mui/material";
+import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Navbar, SideBar } from "../../components/index";
+
+const Layout = () => {
+  const isNonMobile = useMediaQuery("(min-width:600px) ");
+
+  const [isSideBarOpen, setIsSideBarOpen] = useState(true);
+
+  return (
+    <Box display={isNonMobile ? "flex" : "block "} width="100%" height="100%">
+      <SideBar
+        isNonMobile={isNonMobile}
+        drawerWidth="250px"
+        isSideBarOpen={isSideBarOpen}
+        setIsSideBarOpen={setIsSideBarOpen}
+      />
+
+      <Box>
+        <Navbar
+          isSideBarOpen={isSideBarOpen}
+          setIsSideBarOpen={setIsSideBarOpen}
+        />
+        <Outlet />
+      </Box>
+    </Box>
+  );
+};
+()
+export default Layout;
